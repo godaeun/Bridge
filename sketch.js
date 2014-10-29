8accd207f144b069aecc93fb943a72f12c7b91a3
godaeun <gowooda@naver.com>
2014-10-29 15:50:03 +0900

    gg

--- sketch.js
+++ sketch.js
@@ -35,15 +35,7 @@ function setup(){
 
 }
 
-function mousePressed() {
-  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-    song.stop();
-    background(255,0,0);
-  } else {
-    song.play();
-    background(0,255,0);
-  }
-}
+
 function draw(){
    background(3,3,3);  
 
